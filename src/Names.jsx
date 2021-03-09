b89a470fcf6256b761c5663d27f5bb2daf4a252c
SameerBunty <50738759+SAMEERBALANAVAR@users.noreply.github.com>
2021-03-09 11:03:27 +0530

    ntng

--- src/Names.jsx
+++ src/Names.jsx
@@ -7,7 +7,7 @@ function Names(props) {
     <div>
       <h1>My first name is {props.first_name}</h1>
       <h1>My Last name is {props.last_name}</h1>
-      <p>And i am {props.age} years old</p>
+      <h1>And i am {props.age} years old</h1>
     </div>
   );
 }
